1e734c7db4e066465b761b6063438b0870cab659
Jeff Benton <sikletz@gmail.com>
2019-02-18 22:34:17 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,6 +7,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) if !backer.backed_projects.inlude?(self)
+    backer.back_project(self) if !backer.backed_projects.include?(self)
   end
 end
